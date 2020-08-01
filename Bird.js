651e151fe1f6bd961ff83b75221024254b820d29
smayana024436 <66475590+smayana024436@users.noreply.github.com>
2020-08-01 12:55:22 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,15 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    var options ={
+      'density': 1.5
+    }
+    super(x,y,50,50, options);
+    this.image = loadImage("sprites/bird.png");
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+  }
+}
